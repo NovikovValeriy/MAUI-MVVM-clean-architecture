ab5e85da34d8b47dc3a42a40ca4c56893bead6b2
NovikovValeriy <novikov.valera2005@gmail.com>
2024-04-22 23:23:44 +0300

    Initial commit

---
+++ 253504Novikov.UI/App.xaml.cs
@@ -0,0 +1,12 @@
+﻿namespace _253504Novikov.UI
+{
+    public partial class App : Application
+    {
+        public App()
+        {
+            InitializeComponent();
+
+            MainPage = new AppShell();
+        }
+    }
+}
